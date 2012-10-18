1a08b2b81413044f87e0cd7df0c8705f10b9fe28
Ferran Segarra <ferransegarra@gmail.com>
2012-10-18 12:57:10 +0200

    V1

---
+++ helloworld/src/dsa/helloworld/helloworld.java
@@ -0,0 +1,17 @@
+package dsa.helloworld;
+
+public class helloworld {
+
+	public helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world! SI??");
+	}
+
+}
