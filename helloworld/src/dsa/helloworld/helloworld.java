eda98f164cd8de48942c7e6af0932599215886d3
FRG <alexcucu_1992@hotmail.com>
2012-10-18 13:15:56 +0200

    Añadida funcion adios

--- helloworld/src/dsa/helloworld/helloworld.java
+++ helloworld/src/dsa/helloworld/helloworld.java
@@ -9,6 +9,11 @@ public class helloworld {
 	/**
 	 * @param args
 	 */
+	public static void adios()
+	{
+		System.out.println("Bye World! NOOO?");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world! SI??");
